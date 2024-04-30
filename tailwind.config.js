0bf78e2727af4422b8881b86333066a4ff9489ff
MohdFahd <mohammed.f.alsulami@gmail.com>
2024-04-30 15:03:58 +0300

    Updates the new changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
       center: true,
     },
     extend: {},
+    fontSize: {
+      sm: '13px',
+      base: '15px',
+      xl: '19px',
+      lg: '17px',
+      '2xl': '23px',
+      '3xl': '29px',
+      '4xl': '35px',
+      '5xl': '47px',
+    }
   },
   daisyui: {
     themes: ["light"],
